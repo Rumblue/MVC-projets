df4955a3c5612a0fcbf85dc2c3af0507c6dbed4a
Youssef Rumblue <youssef_1804@hotmail.fr>
2023-02-09 14:26:38 +0100

    add DB1.1

--- config.php
+++ config.php
@@ -4,6 +4,6 @@
 const DB_HOST = "localhost";
 const DB_LOGIN = "root";
 const DB_PWD = "";
-const DB_NAME = "mvcprojets";
+const DB_NAME = "mvcprojets_youssef";
 const DB_PORT = "git 3307";
 const DB_CHARSET = "utf8mb4";
